465bb397d223af32be01d8b23617569f8e9f4167
fducat18 <francois.ducatillon@decathlon.com>
2025-02-02 15:45:57 +0100

    add preload image function

--- static/js/header-image.js
+++ static/js/header-image.js
@@ -5,20 +5,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const smallImg = headerImage.getAttribute("small-Image");
   const largeImg = headerImage.getAttribute("large-Image");
 
-  // Set default background image (medium screen)
+  // Function to dynamically preload the right image based on screen size
+  function preloadBackgroundImage() {
+    let preloadImage;
+
+    // Determine the appropriate image to preload based on viewport width
+    if (window.innerWidth <= 480) {
+      preloadImage = smallImg;
+    } else {
+      preloadImage = largeImg;
+    }
+
+    // Create a link element to preload the image
+    const preloadLink = document.createElement("link");
+    preloadLink.rel = "preload";
+    preloadLink.href = preloadImage;
+    preloadLink.as = "image";
+    preloadLink.fetchpriority = "high";
+    preloadLink.type = "image/webp";
+
+    // Append the preload link to the head of the document
+    document.head.appendChild(preloadLink);
+  }
+
+  preloadBackgroundImage();
+
+  // Set default background image (small screen)
   headerImage.style.backgroundImage = `url(${smallImg})`;
 
   // Change background image based on screen width
   function updateBackgroundImage() {
+    let backgroundImage;
     if (window.innerWidth <= 480) {
-      headerImage.style.backgroundImage = `url(${smallImg})`;
+      backgroundImage = smallImg;
     } else {
-      headerImage.style.backgroundImage = `url(${largeImg})`;
+      backgroundImage = largeImg;
     }
+    headerImage.style.backgroundImage = `url(${backgroundImage})`;
   }
-
-  // Initial check
-  updateBackgroundImage();
 
   // Update background image on window resize
   window.addEventListener("resize", updateBackgroundImage);
