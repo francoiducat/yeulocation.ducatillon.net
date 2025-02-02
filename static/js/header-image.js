5947d645d4f1f56ff44b3ae70cadcdb356017a25
fducat18 <francois.ducatillon@decathlon.com>
2025-02-02 18:19:42 +0100

    add loader on header image, hide it when loaded

--- static/js/header-image.js
+++ static/js/header-image.js
@@ -1,33 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const headerImage = document.querySelector(".headerImage");
+  const loader = document.querySelector(".loader");
 
   // Get image URLs from the data attributes
   const smallImg = headerImage.getAttribute("small-Image");
   const largeImg = headerImage.getAttribute("large-Image");
-
-  // Function to dynamically preload the right image based on screen size
-  function preloadBackgroundImage() {
-    let preloadImage;
-
-    // Determine the appropriate image to preload based on viewport width
-    if (window.innerWidth <= 480) {
-      preloadImage = smallImg;
-    } else {
-      preloadImage = largeImg;
-    }
-
-    // Create a link element to preload the image
-    const preloadLink = document.createElement("link");
-    preloadLink.rel = "preload";
-    preloadLink.href = preloadImage;
-    preloadLink.as = "image";
-    preloadLink.type = "image/webp";
-
-    // Append the preload link to the head of the document
-    document.head.appendChild(preloadLink);
-  }
-
-  preloadBackgroundImage();
 
   // Set default background image (small screen)
   headerImage.style.backgroundImage = `url(${smallImg})`;
@@ -40,10 +17,19 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       backgroundImage = largeImg;
     }
+
     headerImage.style.backgroundImage = `url(${backgroundImage})`;
+
+    const img = new Image();
+    img.src = backgroundImage;
+    img.onload = function () {
+      // Once the background image is loaded, hide the loader
+      loader.style.display = "none";
+    };
   }
 
   updateBackgroundImage();
+
   // Update background image on window resize
   window.addEventListener("resize", updateBackgroundImage);
 });
